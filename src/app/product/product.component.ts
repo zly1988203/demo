7c9be73a9c551e4bf1ca4bc50d6ae276ccceb511
zhaoly <zhaoly@okdeer.com>
2017-08-11 10:07:58 +0800

    Initial commit

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  goodsList = [{
+    img:"",
+    name:"",
+    price:"",
+    des:""
+  }]
+}
